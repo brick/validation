83afa593a0141171df11a915f2d352ddb27b32c0
peter279k <peter279k@gmail.com>
2018-02-25 23:15:33 +0800

    test enhancement

---
+++ tests/Internal/DecimalTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Brick\Validation\Tests\Internal;
+
+use Brick\Validation\Internal\Decimal;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for class Decimal.
+ */
+class DecimalTest extends TestCase
+{
+    /**
+     * @expectedException              \InvalidArgumentException
+     * @expectedExceptionMessageRegExp /Decimal number out of range for this platform: \d+/
+     */
+    public function testParseWithValueIsOutOfRange()
+    {
+        Decimal::parse('1000000000000000000000000');
+    }
+
+    /**
+     * @expectedException        \RangeException
+     * @expectedExceptionMessage The subtraction result overflows.
+     */
+    public function testMinusWithSubstractionResultOverflows()
+    {
+        $decimal = Decimal::parse('9223372036854775807');
+        $that = Decimal::parse('-92233720368547758');
+        $decimal->minus($that);
+    }
+
+    /**
+     * @expectedException        \RangeException
+     * @expectedExceptionMessage The scaled result overflows.
+     */
+    public function testScaleValuesWithScaledOverflows()
+    {
+        $decimal = Decimal::parse('100200000300004999.3');
+        $that = Decimal::parse('-2.33333');
+        $decimal->minus($that);
+    }
+}
---
+++ tests/StringValidatorTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Brick\Validation\Tests;
+
+use Brick\Validation\Validator\StringValidator;
+
+/**
+ * Unit tests for the String validator.
+ */
+class StringValidatorTest extends AbstractTestCase
+{
+    public function testStringValidator()
+    {
+        $validator = new StringValidator();
+
+        $this->doTestValidator($validator, [
+            ''   => [],
+            null => [],
+        ]);
+    }
+
+    public function testStringValidatorWithTooShortString()
+    {
+        $validator = new StringValidator(5, 0);
+
+        $this->doTestValidator($validator, [
+            '123' => ['validator.string.too-short'],
+        ]);
+    }
+
+    public function testStringValidatorWithTooLongString()
+    {
+        $validator = new StringValidator(5, 10);
+
+        $this->doTestValidator($validator, [
+            '12345678910' => ['validator.string.too-long'],
+        ]);
+    }
+
+    public function testStringValidatorWithNonUtf8String()
+    {
+        $validator = new StringValidator();
+
+        $this->doTestValidator($validator, [
+            file_get_contents(__DIR__ . '/non_utf8.txt') => ['validator.string.encoding'],
+        ]);
+    }
+
+    public function testGetPossibleMessages()
+    {
+        $validator = new StringValidator();
+        $result = $validator->getPossibleMessages();
+
+        $this->assertArrayHasKey('validator.string.encoding', $result);
+        $this->assertArrayHasKey('validator.string.too-short', $result);
+        $this->assertArrayHasKey('validator.string.too-long', $result);
+        $this->assertContains('The string is not valid UTF-8.', $result);
+        $this->assertContains('The string is too short.', $result);
+        $this->assertContains('The string is too long.', $result);
+    }
+}
